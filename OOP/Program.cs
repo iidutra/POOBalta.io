fb9947fcdbe6e8566382412ecb24fcc5edbbe1a6
iidutra <igordutra07@gmail.com>
2021-02-24 19:36:20 -0300

    first commit

---
+++ OOP/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Balta.ContentContext;
+
+namespace Balta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var articles = new List<Article>();
+           articles.Add(new Article("Artigo sobre OOP", "orientacao-objetos"));
+
+           foreach (var article in articles)
+           {
+               Console.WriteLine(article.Title);
+               Console.WriteLine(article.Url);
+           }
+        }
+    }
+}
